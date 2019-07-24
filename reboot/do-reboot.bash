31513cc4df6f1e10524fa5674c06f65815d5b03f
清水 美里 <mi-shimizu@sakura.ad.jp>
2019-07-24 16:50:41 +0900

    move test-rb.bash to reboot directory

--- reboot/do-reboot.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -u
-
-file="/var/log/load/do-reboot.log"
-date_form=`date '+[%F %R:%S]'`
-msg=`cat /sys/devices/system/clocksource/clocksource0/available_clocksource`
-
-echo "${date_form} clocksource: $msg" >> $file
-
-reboot
--- reboot/readme.txt
+++
@@ -1,2 +0,0 @@
-'do-reboot.bash' is a script which make the system shutdown. 
-Be caresul when you try to execute this script.
