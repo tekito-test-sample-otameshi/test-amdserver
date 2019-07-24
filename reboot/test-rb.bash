258780e85af02c145a725c2b40011285fb559b6b
清水 美里 <mi-shimizu@sakura.ad.jp>
2019-07-24 17:16:59 +0900

    add 	conditional jump

--- reboot/test-rb.bash
+++ reboot/test-rb.bash
@@ -4,7 +4,14 @@ DATE=`date '+%F %R'`
 F_DATE=`date '+%Y%m%d'`
 FILE="/var/log/load/reboot_dir/test-rb_${F_DATE}.log"
 
-CLK=`cat /sys/devices/system/clocksource/clocksource0/current_clocksource`
-echo "[$DATE]: $CLK" >> $FILE
+num_line=`wc -l $FILE`
 
-reboot
+if [ $num_line -lt 100 ]
+then
+    CLK=`cat /sys/devices/system/clocksource/clocksource0/current_clocksource`
+    echo "[$DATE]: $CLK" >> $FILE
+
+    reboot
+else
+    echo "already rebooted 100 times. finished this script." >> $FILE
+fi
