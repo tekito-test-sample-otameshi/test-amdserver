8e5b99c8a423575f48ceb7fdd0bb21bc8c9d7eda
mi-shimizu-sakura <mi-shimizu@sakura.ad.jp>
2019-07-24 17:42:34 +0900

    devided aging.bash to high and idle

---
+++ aging/high/aging.bash
@@ -0,0 +1,34 @@
+#!/bin/bash -u
+
+# Description
+#   Main script of aging test.
+#   You need other scripts below when executing this script.
+#        + measure.bash
+#        + get-ipmi.bash
+
+test_time="86400"
+
+## get info
+#thread=`grep '^processor[^:]*:' /proc/cpuinfo | wc -l`
+#mem=`grep --max-count=1 '^MemTotal:' /proc/meminfo | awk '{ print $2 }'`
+#mem_90=`echo "${mem} * 0.9" | bc | cut -d. -f1`
+
+# main command
+#LOAD_CMS="stress --cpu ${thread} --vm 1 --vm-keep --vm-bytes ${mem_90} --hdd 2 --hdd-bytes 10G --timeout ${test_time}s &" 
+IPMI_VAL="/var/tmp/load/aging/get-ipmi.bash"
+
+MEASURE () {
+    measure="/var/tmp/load/aging/measure.bash"
+
+    for i in {1..2}
+    do
+        eval ${measure}
+        sleep 43200
+    done
+}
+
+# main process
+sleep 86400 & sleep 300 
+eval ${IPMI_VAL} & MEASURE
+
+exit 0
