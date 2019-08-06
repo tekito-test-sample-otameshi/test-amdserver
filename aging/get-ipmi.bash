8ab85a7e0d3df2bde0617a3852c50e3db333fdd8
清水 美里 <mi-shimizu@sakura.ad.jp>
2019-08-06 11:19:14 +0900

    change the command to join the two files

--- aging/get-ipmi.bash
+++ aging/get-ipmi.bash
@@ -13,7 +13,7 @@ do
     #log_date=`date '+%Y%m%d'`
     log_time=`date '+%H%M'`
 
-    echo "[YYYYMMDD] | [${log_time}]" > $tmp_file
+    echo "[ITEM] | [${log_time}]" > $tmp_file
     ipmitool sdr list full | awk -F'|' -v OFS='|' '{print $1,$2}' | sort >> $tmp_file
 
     if [ -s $file ]
@@ -21,8 +21,10 @@ do
         # If the file is not empty (already the result is written),
         cp_file=`mktemp /home/load/ipmi_tmp.XXX`    
 
-	    cat  $file > $cp_file
-        join -t '|' $cp_file $tmp_file > $file
+	    #cat  $file > $cp_file
+	    awk -F'|' -v OFS='|' '{print $2}' $tmp_file > $cp_file
+        #join -t '|' $cp_file $tmp_file > $file
+        paste -d '|' $file $cp_file
 
         rm -f $cp_file
     else
